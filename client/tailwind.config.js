07c43247387a8aa223d6acf8c2dcbfab73549b42
Hyrum Dolan <hyrumdolan@Hyrums-MacBook-Pro.local>
2024-03-21 22:31:01 -0600

    first commit

---
+++ client/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"]
+
+export default {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
