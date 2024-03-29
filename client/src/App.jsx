557cff7318f19397af948fb04afb550a0f56403c
Rocky <alexwaldmann2004@gmail.com>
2024-03-28 21:18:20 -0600

    Tailwind setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,42 +1,51 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <button
-        onClick={async () => {
-          const res = await fetch("/hello");
-          console.log(await res.json());
-        }}
-      >
-        Click to fetch server
-      </button>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="flex min-h-[100vh] min-w-[100vw] justify-center bg-red-500 text-white">
+      <div className="size-screen flex flex-col justify-center text-center align-middle">
+        <button
+          onClick={async () => {
+            const res = await fetch("/hello");
+            console.log(await res.json());
+          }}
+        >
+          Click to fetch server
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+        <div className="flex flex-col items-center gap-4 md:flex-row md:justify-center md:align-middle">
+          <a href="https://vitejs.dev" target="_blank">
+            <img
+              src={viteLogo}
+              className="drop-shadow-glow size-16"
+              alt="Vite logo"
+            />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img
+              src={reactLogo}
+              className="animate-spin-slow drop-shadow-glow size-16"
+              alt="React logo"
+            />
+          </a>
+        </div>
+        <h1>Vite + React</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   );
 }
 
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src{/**/*,/*}.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "spin-slow": "spin 10s linear infinite"
+      },
+      dropShadow: {
+        glow: [
+          "0 0px 20px rgba(255,255, 255, 0.35)",
+          "0 0px 65px rgba(255, 255,255, 0.2)"
+        ]
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
