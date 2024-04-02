978c8faf4f7062f2d8e9602658fd994426e5dc10
landonkimber <landonkimber33@gmail.com>
2024-04-01 19:59:10 -0600

    rewrote main.jsx to use react router and apollo client

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,34 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./tailwind.css";
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import App from './App.jsx';
+
+import LoginPage from './pages/LoginPage.jsx';
+import HomePage from './pages/HomePage.jsx';
+import SignupPage from './pages/SignupPage.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <h1 className='display-2'>Wrong page!</h1>,
+    children: [
+      {
+        index: true,
+        element: <LoginPage />
+      }, {
+        path: '/home',
+        element: <HomePage />
+      }, {
+        path: '/signup',
+        element: <SignupPage />
+      }
+    ]
+  }
+]);
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
+  <RouterProvider router={router} />
 );
