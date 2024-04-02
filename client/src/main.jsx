5abecb33b5a025feb4ebc094cbefa6fb08aae2c2
Ryan Darton <ryan@timemachinevideo.com>
2024-04-01 23:29:36 -0600

    Fix a runtime error in console.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import 'regenerator-runtime/runtime';
 
 import App from './App.jsx';
 
