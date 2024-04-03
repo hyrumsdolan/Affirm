c18cc956cd01ca9b8f6f852aa90bc6751863b8ce
Hyrum Dolan <hyrumdolan@Hyrums-MacBook-Pro.local>
2024-04-02 19:12:54 -0600

    apollo server connect to client

---
+++ client/src/utils/apolloClient.js
@@ -0,0 +1,40 @@
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { onError } from '@apollo/client/link/error';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.forEach(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      )
+    );
+  if (networkError) console.log(`[Network error]: ${networkError}`);
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(errorLink).concat(httpLink),
+  cache: new InMemoryCache(),
+  onResponse: ({ response }) => {
+    if (response.data && response.data.loginUser && response.data.loginUser.token) {
+      const token = response.data.loginUser.token;
+      localStorage.setItem('id_token', token);
+    }
+  },
+});
+
+export default client;
