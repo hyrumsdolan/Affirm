522022334d40ea7394ceff0f79fc321cf85ddca8
Hyrum Dolan <hyrumdolan@Hyrums-MacBook-Pro.local>
2024-04-03 19:22:11 -0600

    Claude connect to frontend + add compononet to pages

---
+++ client/src/utils/callClaude.js
@@ -0,0 +1,30 @@
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
+import { CALL_CLAUDE } from './mutations';
+import  client  from './apolloClient';
+
+export const sendToClaude = async (input) => {
+  try {
+    console.log("Sending to Claude:", input);
+
+    const { data } = await client.mutate({
+      mutation: CALL_CLAUDE,
+      variables: { input },
+    });
+
+    const result = data.callClaude;
+    localStorage.setItem("claudeResponse", result);
+    return result;
+  } catch (error) {
+    console.error("Error:", error);
+    throw error;
+  }
+};
+
+export const getClaudeResponse = (startIndex = 0, endIndex = undefined) => {
+  const storedResponse = localStorage.getItem("claudeResponse");
+  if (storedResponse) {
+    const dreamsList = storedResponse.split(" | ");
+    return dreamsList.slice(startIndex, endIndex);
+  }
+  return [];
+};
