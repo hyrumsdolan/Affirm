378b0df073547ea0c9174b8685ebd048096c6f4b
Ryan Darton <ryan@timemachinevideo.com>
2024-04-06 13:33:34 -0600

    Update subtext on login page.

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -7,21 +7,18 @@ import InputBox from "../components/InputBox";
 import Button from "../components/Button";
 import Auth from "../utils/auth";
 
-
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: "", password: "" });
   const [showAlert, setShowAlert] = useState(false);
   const [loginUser] = useMutation(LOGIN_USER);
   const navigate = useNavigate();
 
-
-
-  const handleInputChange = (event) => {
+  const handleInputChange = event => {
     const { name, value } = event.target;
     setUserFormData({ ...userFormData, [name]: value });
   };
 
-  const handleFormSubmit = async (event) => {
+  const handleFormSubmit = async event => {
     event.preventDefault();
 
     // check if form has everything (as per react-bootstrap docs)
@@ -32,10 +29,10 @@ const LoginForm = () => {
       try {
         console.log(userFormData);
         const { data } = await loginUser({
-          variables: { ...userFormData },
+          variables: { ...userFormData }
         });
         console.log("look here FOR DATA");
-    
+
         Auth.login(data.loginUser.token);
 
         setUserFormData({ email: "", password: "" });
@@ -50,67 +47,75 @@ const LoginForm = () => {
 
   return (
     <>
-              <div className="h-full flex flex-col md:flex-row">
-          <div className="w-full md:w-3/5 flex flex-col items-center justify-center relative">
-            
-            <h2 className="text-3xl md:text-5xl font-bold mb-4 text-center">let's improve together</h2>
-            <p className="text-center">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-              vitae elit libero, a pharetra augue.
-            </p>
-          </div>
-          <div className="w-full md:w-2/5 flex flex-col items-center justify-center border-t md:border-t-0 md:border-l border-gray-200 p-8">
-            <form onSubmit={handleFormSubmit} className="w-full md:w-9/12">
-              <div className="mb-4">
-                <label htmlFor="email" className="block text-gray-700 font-bold mb-2">
-                  Email:
-                </label>
-                <InputBox
-                  type="email"
-                  id="email"
-                  name="email"
-                  value={userFormData.email}
-                  onChange={handleInputChange}
-                  required
-                  className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                />
-              </div>
-              <div className="mb-6">
-                <label htmlFor="password" className="block text-gray-700 font-bold mb-2">
-                  Password:
-                </label>
-                <InputBox
-                  type="password"
-                  id="password"
-                  name="password"
-                  value={userFormData.password}
-                  onChange={handleInputChange}
-                  required
-                  className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                />
-              </div>
-              <Button type="submit" 
-              className="w-full"
-              navigateTo="/ten-year-dream">
-                Login
-              </Button>
-            </form>
-            <p className="mt-4 text-center">
-              Don't have an account?{" "}
-              <a href="/signup" className="text-blue-500 hover:text-blue-700">
-                Sign up
-              </a>
-            </p>
-            {showAlert && (
-              <div className="mt-4 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded">
-                Sign in failed
-              </div>
-            )}
-          </div>
+      <div className="flex h-full flex-col md:flex-row">
+        <div className="relative flex w-full flex-col items-center justify-center md:w-3/5">
+          <h2 className="mb-4 text-center text-3xl font-bold md:text-5xl">
+            let's improve together
+          </h2>
+          <p className="text-center">
+            An application to help you dream big, and stay on course to live
+            your best life.
+          </p>
         </div>
-      
+        <div className="flex w-full flex-col items-center justify-center border-t border-gray-200 p-8 md:w-2/5 md:border-l md:border-t-0">
+          <form onSubmit={handleFormSubmit} className="w-full md:w-9/12">
+            <div className="mb-4">
+              <label
+                htmlFor="email"
+                className="mb-2 block font-bold text-gray-700"
+              >
+                Email:
+              </label>
+              <InputBox
+                type="email"
+                id="email"
+                name="email"
+                value={userFormData.email}
+                onChange={handleInputChange}
+                required
+                className="w-full rounded-md border border-gray-300 px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </div>
+            <div className="mb-6">
+              <label
+                htmlFor="password"
+                className="mb-2 block font-bold text-gray-700"
+              >
+                Password:
+              </label>
+              <InputBox
+                type="password"
+                id="password"
+                name="password"
+                value={userFormData.password}
+                onChange={handleInputChange}
+                required
+                className="w-full rounded-md border border-gray-300 px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </div>
+            <Button
+              type="submit"
+              className="w-full"
+              navigateTo="/ten-year-dream"
+            >
+              Login
+            </Button>
+          </form>
+          <p className="mt-4 text-center">
+            Don't have an account?{" "}
+            <a href="/signup" className="text-blue-500 hover:text-blue-700">
+              Sign up
+            </a>
+          </p>
+          {showAlert && (
+            <div className="mt-4 rounded border border-red-400 bg-red-100 px-4 py-3 text-red-700">
+              Sign in failed
+            </div>
+          )}
+        </div>
+      </div>
     </>
   );
-}
+};
 
 export default LoginForm;
