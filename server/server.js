737d8cfd68403f5678cfb522ea94787aaf99ac94
Hyrum Dolan <hyrumdolan@Hyrums-MacBook-Pro.local>
2024-04-06 18:28:29 -0600

    updated buttons

--- server/server.js
+++ server/server.js
@@ -5,8 +5,7 @@ const path = require("path");
 const { authMiddleware } = require("./utils/auth");
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
-const auth = require("./utils/auth");
-const { User } = require("./models");
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
