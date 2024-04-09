497b43675b1b4998eab22da0993dbd89d11e2fa7
Rocky <alexwaldmann2004@gmail.com>
2024-04-08 22:40:22 -0600

    Button resizing on AndNext page

--- client/src/pages/AndNext.jsx
+++ client/src/pages/AndNext.jsx
@@ -14,10 +14,16 @@ const AndNext = ({ user }) => {
   const [addLittleDreams] = useMutation(ADD_LITTLE_DREAMS);
   const [isLoading, setIsLoading] = useState(false);
   const handleMutationCompleted = useUserNavigation();
+  const [buttonHeightUpdated, setButtonHeightUpdated] = useState(false);
+  let longest = "";
 
   useEffect(() => {
     const fetchedDreams = getClaudeResponse(); // Core dream is last element
     setDreams(fetchedDreams);
+
+    fetchedDreams.forEach(dream => {
+      if (dream.length > longest.length) longest = dream;
+    });
   }, []);
 
   const toggleDreamSelection = (dream, isSelected) => {
@@ -43,31 +49,56 @@ const AndNext = ({ user }) => {
     }
   };
 
+  useEffect(() => {
+    setTimeout(() => {
+      const allSelectableButtons =
+        document.querySelectorAll(".selectableButton");
+
+      let buttonHeight = 0;
+      allSelectableButtons.forEach(button => {
+        if (button.children[0].textContent === longest) {
+          buttonHeight = button.clientHeight;
+        }
+      });
+
+      const heightClass = `h-[${buttonHeight}px]`;
+      allSelectableButtons.forEach(button => {
+        button.classList.replace("h-auto", heightClass);
+      });
+    }, 0);
+  }, []);
+
   return (
     <div className="">
       <main className="grid grid-cols-1 justify-around gap-1 md:grid-cols-2 lg:grid-cols-3">
         {dreams.map((dream, index) => (
           <div className="m-10" key={index}>
             <SelectableButton
+              className="selectableButton"
+              id={index}
               initialText={dream}
               onSelect={isSelected => toggleDreamSelection(dream, isSelected)}
             />
           </div>
         ))}
       </main>
-      <Button
-        onClick={handleSave}
-        className="r-0 absolute right-0 m-10"
-        user={user}
-        saveToUser="littledreams"
-        isEnabled={Object.values(selectedDreams).some(isSelected => isSelected)}
-        inputForDBSave={Object.keys(selectedDreams).filter(
-          dream => selectedDreams[dream]
-        )}
-        onMutationCompleted={handleMutationCompleted}
-      >
-        save & continue
-      </Button>
+      <div className="flex w-screen justify-end">
+        <Button
+          onClick={handleSave}
+          className="bottom-10 left-0 m-10"
+          user={user}
+          saveToUser="littledreams"
+          isEnabled={Object.values(selectedDreams).some(
+            isSelected => isSelected
+          )}
+          inputForDBSave={Object.keys(selectedDreams).filter(
+            dream => selectedDreams[dream]
+          )}
+          onMutationCompleted={handleMutationCompleted}
+        >
+          save & continue
+        </Button>
+      </div>
     </div>
   );
 };
